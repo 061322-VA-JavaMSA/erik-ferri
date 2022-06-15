9ca29c355f8932e68899146d7e376233416b5fca
EFerri222 <eferri222@gmail.com>
2022-06-15 17:22:09 -0400

    Added Scanner activity

---
+++ assignments/week1/scanner-activity/src/main/java/com/revature/ScannerDriver.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+import java.util.Scanner;
+
+public class ScannerDriver {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome! Please enter a number between 1 and 3.");
+		
+	}
+}
