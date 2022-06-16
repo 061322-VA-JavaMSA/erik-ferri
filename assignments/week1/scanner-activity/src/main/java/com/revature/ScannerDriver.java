a1896acc9a1fef2721e9da68c06dab64fc0c7738
EFerri222 <eferri222@gmail.com>
2022-06-15 22:57:04 -0400

    Updated Scanner activity

--- assignments/week1/scanner-activity/src/main/java/com/revature/ScannerDriver.java
+++ assignments/week1/scanner-activity/src/main/java/com/revature/ScannerDriver.java
@@ -7,8 +7,45 @@ public class ScannerDriver {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		
 		System.out.println("Welcome! Please enter a number between 1 and 3.");
+		System.out.println("1: Get a random number between 1 and 100");
+		System.out.println("2: Reverse a String");
+		System.out.println("3: Exit application");
+		String userInput = scan.nextLine();
+		String reversedString = "";
 		
+		while(!userInput.equals("3")) {
+			if(userInput.equals("1")) {
+//				Prints random number between 1 and 100, parses to integer
+				System.out.println("Random number: " + (int)Math.floor(Math.random()*100 + 1));
+				System.out.println("-------------");
+				System.out.println("1: Get a random number between 1 and 100");
+				System.out.println("2: Reverse a String");
+				System.out.println("3: Exit application");
+				userInput = scan.nextLine();
+			} else if(userInput.equals("2")){
+				System.out.println("Type the String you would like reversed:");
+				userInput = scan.nextLine();
+				for(int i = userInput.length() - 1; i >= 0; i--) {
+					reversedString += userInput.charAt(i);
+				}
+				System.out.println(reversedString);
+				System.out.println("-------------");
+				System.out.println("1: Get a random number between 1 and 100");
+				System.out.println("2: Reverse a String");
+				System.out.println("3: Exit application");
+				userInput = scan.nextLine();
+			} else {
+				System.out.println("Invalid entry. Try again.");
+				System.out.println("-------------");
+				System.out.println("1: Get a random number between 1 and 100");
+				System.out.println("2: Reverse a String");
+				System.out.println("3: Exit application");
+				userInput = scan.nextLine();
+			}
+		}
+		
+		scan.close();
+
 	}
 }
