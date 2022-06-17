1325fb6e31236196018d3b6fe262c642cb586531
EFerri222 <eferri222@gmail.com>
2022-06-17 18:03:07 -0400

    Added calculator-test-activity

---
+++ assignments/week1/calculator-test-activity/src/main/java/com/revature/Calculator.java
@@ -0,0 +1,22 @@
+package com.revature;
+
+//import com.revature.exceptions.Number13Exception;
+
+public class Calculator {
+
+	
+	public int add(int a, int b) {
+		if(a == 8 && b == 5 ) {
+//			throw new Number13Exception();
+		}
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int sumOfAnArray(int[] intArr) {
+		return 0;
+	}
+}
