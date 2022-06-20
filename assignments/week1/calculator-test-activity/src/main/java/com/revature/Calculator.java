1dc352a4ce2761a3a4ba7d03eec233db79d66045
EFerri222 <eferri222@gmail.com>
2022-06-19 21:57:18 -0400

    Updated calculator-test-activity

--- assignments/week1/calculator-test-activity/src/main/java/com/revature/Calculator.java
+++ assignments/week1/calculator-test-activity/src/main/java/com/revature/Calculator.java
@@ -1,19 +1,26 @@
 package com.revature;
 
-//import com.revature.exceptions.Number13Exception;
+import com.revature.exceptions.Number13Exception;
 
 public class Calculator {
-
 	
 	public int add(int a, int b) {
-		if(a == 8 && b == 5 ) {
-//			throw new Number13Exception();
+		if(a + b == 13) {
+			throw new Number13Exception();
 		}
 		return a + b;
 	}
 	
 	public int subtract(int a, int b) {
 		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
 	}
 	
 	public int sumOfAnArray(int[] intArr) {
--- assignments/week1/calculator-test-activity/src/main/java/com/revature/exceptions/Number13Exception.java
+++ assignments/week1/calculator-test-activity/src/main/java/com/revature/exceptions/Number13Exception.java
@@ -1,5 +1,8 @@
 package com.revature.exceptions;
 
 public class Number13Exception extends ArithmeticException{
-
+	public Number13Exception() {
+//		super(errorMessage);
+//		throw new Number13Exception();
+	}
 }
