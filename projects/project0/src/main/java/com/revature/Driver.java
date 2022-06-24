e84d53e67d0a87313b65e44043f049785c896144
EFerri222 <eferri222@gmail.com>
2022-06-23 20:10:17 -0400

    project0: Set up UserDAO and UserPostgres files, created a test User from Driver class

--- projects/project0/src/main/java/com/revature/Driver.java
+++ projects/project0/src/main/java/com/revature/Driver.java
@@ -1,11 +1,13 @@
 package com.revature;
 
+import java.util.List;
 import java.util.Scanner;
 
 //import com.revature.exceptions.*;
 import com.revature.models.*;
 //import com.revature.services.*;
 //import java.sql.Connection;
+import com.revature.daos.*;
 
 public class Driver {
 	
@@ -16,6 +18,11 @@ public class Driver {
 //		displayInstrumentMenu();
 //		handleInstrumentSelection();
 //		printUser();
+		UserPostgres up = new UserPostgres();
+		User userTBC = new User();
+		userTBC.setUsername("Test Username");
+		userTBC.setPassword("Test Password");
+		up.createUser(userTBC);
 		
 	}
 	
@@ -62,58 +69,3 @@ public class Driver {
 //		}
 //	}
 }
-
-//public class Driver {
-//	
-//	static Scanner scan;
-//	static AuthService as;
-//	static UserService us;
-//	static TaskService ts;
-//	
-//	public static void main(String[] args) {
-//		scan = new Scanner(System.in);
-//		as = new AuthService();
-//		us = new UserService();
-//		ts = new TaskService();
-//		
-//		String username = null;
-//		String password = null;
-//		
-//		System.out.println("Please enter username:");
-//		username = scan.nextLine();
-//		System.out.println("Please enter password:");
-//		password = scan.nextLine();
-//		
-//		try {
-//			System.out.println(as.login(username, password));
-//		} catch (LoginException e) {
-//			System.out.println("Invalid credentials.");
-////			e.printStackTrace();
-//		}
-//
-//		
-//		List<User> users = us.getUsers();
-//		for(User u : users) {
-//			System.out.println(u);
-//		}	
-//		// "1; drop table users"
-//		System.out.println("Create, username:");
-//		String uname = scan.nextLine();
-//		System.out.println("Create, password:");
-//		String pass = scan.nextLine();
-//		User userTBC = new User();
-//		userTBC.setUsername(uname);
-//		userTBC.setPassword(pass);
-//		System.out.println(us.createUser(userTBC));
-//		
-//		System.out.println("Retrieve tasks for which user id?");
-//		int userId = Integer.parseInt(scan.nextLine());
-//		List<Task> tasksForUser = ts.getTasksByUserId(userId);
-//		for(Task t : tasksForUser) {
-//			System.out.println(t);
-//		}
-//		
-//		scan.close();
-//	}
-//
-//}
