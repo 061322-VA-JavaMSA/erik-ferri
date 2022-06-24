85cb720d39f68205aab0f1fb2f7f5a617601f374
EFerri222 <eferri222@gmail.com>
2022-06-24 15:04:48 -0400

    project0: Added LoginView and configurations for handling a User logging in

--- projects/project0/src/main/java/com/revature/views/LoginView.java
+++ projects/project0/src/main/java/com/revature/views/LoginView.java
@@ -2,25 +2,48 @@ package com.revature.views;
 
 import java.util.Scanner;
 
-import com.revature.daos.UserPostgres;
-import com.revature.models.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.revature.exceptions.LoginException;
+//import com.revature.models.User;
+import com.revature.services.AuthService;
+import com.revature.services.UserService;
+//import com.revature.daos.UserPostgres;
 
 public class LoginView {
 
-	static String userInput;
+	static Scanner scan;
+	static AuthService as;
+	static UserService us;
+	private static Logger log = LogManager.getLogger(LoginView.class);
 
 	public static void login() {
-		Scanner scan = new Scanner(System.in);
-		User userTBC = new User();
-		UserPostgres up = new UserPostgres();
+		scan = new Scanner(System.in);
+//		userTTL = user trying to login
+//		User userTTL = new User();
+		as = new AuthService();
+		us = new UserService();
+		
+		String username = null;
+		String password = null;
 		
 		System.out.println("What is your username?");
-		userInput = scan.nextLine();
-		userTBC.setUsername(userInput);
+		username = scan.nextLine();
+//		userTTL.setUsername(username);
 		
 		System.out.println("What is your password?");
-		userInput = scan.nextLine();
-		userTBC.setPassword(userInput);
+		password = scan.nextLine();
+//		userTTL.setPassword(password);
+		
+		try {
+			log.info(as.login(username, password));
+		} catch (LoginException e) {
+			System.out.println("Invalid credentials.");
+			log.error("Login exception was thrown: " + e.fillInStackTrace());
+//			e.printStackTrace();
+		}
+		
 		scan.close();
 	}
 }
