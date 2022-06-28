cded1aa009558d21ecf56e1d4ab83d4f8374f3ae
EFerri222 <eferri222@gmail.com>
2022-06-28 14:34:00 -0400

    project0: Clean up Driver class by removing unneeded commented-out code

--- projects/project0/src/main/java/com/revature/Driver.java
+++ projects/project0/src/main/java/com/revature/Driver.java
@@ -1,41 +1,10 @@
 package com.revature;
 
-//import java.util.List;
-//import java.util.Scanner;
-
 import com.revature.views.*;
-//import com.revature.exceptions.*;
-//import com.revature.models.*;
-//import com.revature.services.*;
-//import java.sql.Connection;
-//import com.revature.daos.*;
 
 public class Driver {
 	
 	public static void main(String[] args) {
-		
-//		handleInstrumentSelection();
 		FrontDoorView.welcome();
-		
 	}
-
-//	
-//	public static void handleInstrumentSelection() {
-//		Scanner scan = new Scanner(System.in);
-//		userInput = scan.nextLine();
-//		if(userInput.equals("1")) {
-//			System.out.println("You are interested in a guitar.");
-//		} else if(userInput.equals("2")){
-//			System.out.println("You are interested in drums.");
-//		} else if(userInput.equals("3")){
-//			System.out.println("You are interested in a microphone.");
-//		} else if(userInput.equals("4")){
-//			System.out.println("Exiting store... Goodbye!");
-//			scan.close();
-//		} else {
-//			System.out.println("Invalid entry. Try again.");
-//			displayInstrumentMenu();
-//			handleInstrumentSelection(); //recursive function
-//		}
-//	}
 }
