e8a3c2cf8847de8346c260a4b7eb7d21562316b1
EFerri222 <eferri222@gmail.com>
2022-07-01 15:35:07 -0400

    Add pokemon-fetch-activity

---
+++ assignments/week3/pokemon-fetch-activity/fetch.js
@@ -0,0 +1,41 @@
+// document.getElementById('getData').onclick = getData;
+document.getElementById('get-data').addEventListener("click", getData);
+
+/*
+    - When button is clicked, send http request to API for a specific id
+
+    - get the id value from input box
+    - send request to PokeAPI
+        - Method: GET
+        - Headers: None
+        - Body: None
+        - url: https://pokeapi.co/api/v2/pokemon/ + id from input box
+    - might have to convert JSON to JS object
+
+    - populate the data in Section
+*/
+let baseApiURL = 'https://pokeapi.co/api/v2/pokemon';
+
+async function getData() {
+    console.log('Button was clicked!');
+    let id = document.getElementById('data-input').value;
+    console.log(`id = ${id}`);
+
+    let httpResponse = await fetch(`${baseApiURL}/${id}`);
+
+    if(httpResponse.status >= 200 && httpResponse.status < 300){
+    let data = await httpResponse.json();
+
+    populateData(data);
+    
+    } else {
+        console.log('Invalid request.');
+    }
+}
+
+function populateData(response) {
+    console.log(response);
+    var pokemon = document.createElement('tr');
+    pokemon.innerHTML = "<td>" + response.id + "</td><td>" + response.name.charAt(0).toUpperCase() + response.name.slice(1) + "</td>";
+    document.getElementById('pokemon-table').appendChild(pokemon);
+}
