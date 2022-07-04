5ffb0dc8cbd67ff2cd915311ca168d6baa5dc95d
EFerri222 <eferri222@gmail.com>
2022-07-03 20:51:12 -0400

    pokemon-fetch-activity: Give users ability to get pokemon by either number or name, add bootstrap table, update styling, add gym leader song

--- assignments/week3/pokemon-fetch-activity/fetch.js
+++ assignments/week3/pokemon-fetch-activity/fetch.js
@@ -1,41 +1,75 @@
-// document.getElementById('getData').onclick = getData;
 document.getElementById('get-data').addEventListener("click", getData);
 
-/*
-    - When button is clicked, send http request to API for a specific id
+// Execute a function when the user presses a key on the keyboard
+var nameInput = document.getElementById('name-input');
+var numberInput = document.getElementById('number-input');
 
-    - get the id value from input box
-    - send request to PokeAPI
-        - Method: GET
-        - Headers: None
-        - Body: None
-        - url: https://pokeapi.co/api/v2/pokemon/ + id from input box
-    - might have to convert JSON to JS object
+nameInput.addEventListener("keypress", function(event) {
+    numberInput.value = "";
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("get-data").click();
+    }
+});
 
-    - populate the data in Section
-*/
+numberInput.addEventListener("keypress", function(event) {
+    nameInput.value = "";
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("get-data").click();
+    }
+});
+
+function clearNumberInput() {
+    numberInput.value = "";
+}
+
+function clearNameInput() {
+    nameInput.value = "";
+}
+
 let baseApiURL = 'https://pokeapi.co/api/v2/pokemon';
 
 async function getData() {
     console.log('Button was clicked!');
-    let name = document.getElementById('data-input').value.toLowerCase();
-    console.log(`name = ${name}`);
 
-    let httpResponse = await fetch(`${baseApiURL}/${name}`);
+    if(!numberInput.value) {
+        let name = document.getElementById('name-input').value.toLowerCase();
+        console.log(`name = ${name}`);
 
-    if(httpResponse.status >= 200 && httpResponse.status < 300){
-    let data = await httpResponse.json();
-
-    populateData(data);
+        let httpResponse = await fetch(`${baseApiURL}/${name}`);
     
+        if(httpResponse.status >= 200 && httpResponse.status < 300){
+        let data = await httpResponse.json();
+    
+        populateData(data);
+        }
     } else {
-        console.log('Invalid request.');
+        let id = document.getElementById('number-input').value;
+        console.log(`id = ${id}`);
+
+        let httpResponse = await fetch(`${baseApiURL}/${id}`);
+    
+        if(httpResponse.status >= 200 && httpResponse.status < 300){
+        let data = await httpResponse.json();
+    
+        populateData(data);
+        }
     }
 }
 
 function populateData(response) {
     console.log(response);
     var pokemon = document.createElement('tr');
-    pokemon.innerHTML = `<td>${response.id}</td><td>${response.name.charAt(0).toUpperCase() + response.name.slice(1)}</td><td><img src=${response.sprites.front_default}></td></td><td><img src=${response.sprites.front_shiny} ></td>`;
-    document.getElementById('pokemon-table').appendChild(pokemon);
+    pokemon.innerHTML = `<th scope="row">${response.id}</th>`
+    pokemon.innerHTML += `<td>${response.name.charAt(0).toUpperCase() + response.name.slice(1)}</td>`;
+    pokemon.innerHTML += `<td><img src=${response.sprites.front_default}></td>`;
+    pokemon.innerHTML += `<td><img src=${response.sprites.front_shiny} ></td>`;
+    document.getElementById('table-body').appendChild(pokemon);
 }
