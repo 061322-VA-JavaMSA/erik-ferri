60e9865b745274876b3427fc351709d0ee02f197
EFerri222 <eferri222@gmail.com>
2022-07-14 16:18:02 -0400

    project1: Allow managers to deny reimbursement requests

--- projects/project1/src/main/webapp/scripts/approveordeny.js
+++ projects/project1/src/main/webapp/scripts/approveordeny.js
@@ -9,7 +9,7 @@ async function getPendingReqs() {
     for (reimbReq in data) {
       let pendingReqItem = document.createElement('li');
       pendingReqItem.classList.add("list-group-item");
-      pendingReqItem.innerHTML = `<span class="check icon-span" onclick="approve(${data[reimbReq].id})"><i class="fas fa-check-square"></i></span><span class="trash icon-span"><i class="fas fa-trash-alt"></i></span>${data[reimbReq].reimbType} - $${data[reimbReq].reimbAmount}: ${data[reimbReq].reimbDescription}           - <span class="submitted-by">Submitted by ${data[reimbReq].userId}</span>`;
+      pendingReqItem.innerHTML = `<span class="check icon-span" onclick="approve(${data[reimbReq].id})"><i class="fas fa-check-square"></i></span><span class="trash icon-span" onclick="deny(${data[reimbReq].id})"><i class="fas fa-trash-alt"></i></span>${data[reimbReq].reimbType} - $${data[reimbReq].reimbAmount}: ${data[reimbReq].reimbDescription}           - <span class="submitted-by">Submitted by ${data[reimbReq].userId}</span>`;
       pendingReqsList.appendChild(pendingReqItem);
     }
   } else {
@@ -23,6 +23,21 @@ async function approve(id) {
   const data = {
     reimbId: id,
     reimbStatus: "approved"
+  }
+  const response = await fetch(`${apiUrl}/reimbursements/?id=${data.reimbId}&reimbStatus=${data.reimbStatus}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+async function deny(id) {
+  const data = {
+    reimbId: id,
+    reimbStatus: "denied"
   }
   const response = await fetch(`${apiUrl}/reimbursements/?id=${data.reimbId}&reimbStatus=${data.reimbStatus}`, {
     method: 'PUT',
