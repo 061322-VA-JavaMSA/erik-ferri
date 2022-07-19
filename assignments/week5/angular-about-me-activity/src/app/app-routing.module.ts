1f7d7583f385dffbb6f440b356dc60e909d9d585
EFerri222 <eferri222@gmail.com>
2022-07-18 20:42:09 -0400

    Update angular-about-me-activity

--- assignments/week5/angular-about-me-activity/src/app/app-routing.module.ts
+++ assignments/week5/angular-about-me-activity/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { TwoTruthsAndALieComponent } from './two-truths-and-a-lie/two-truths-and-a-lie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'two-truths-and-a-lie', component: TwoTruthsAndALieComponent },
+  { path: '', redirectTo: '/about-me', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
